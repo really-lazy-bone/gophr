4b3e9e21dedfa466888c6d41148474dd26e532a3
Eric Liao <rcliao01@gmail.com>
2015-10-24 14:01:57 -0700

    add initial template implementation

--- ionic/www/js/app.js
+++ ionic/www/js/app.js
@@ -1,9 +1,4 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('gophr', ['ionic', 'ngRoute', 'relativeDate'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -16,4 +11,101 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+})
+
+.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'HomeCtrl as home'
+      })
+      .when('/new-purchase', {
+        templateUrl: 'partials/new-purchase.html'
+      })
+      .when('/pickup/:id', {
+        templateUrl: 'partials/pickup.html',
+        controller: 'PickupCtrl as pickup'
+      })
+      .when('/location-picker', {
+        templateUrl: 'partials/location-picker.html',
+        controller: 'LocationPickerCtrl as location'
+      })
+      .otherwise({
+        redirectTo: '/home'
+      });;
+  }])
+
+.controller('LocationPickerCtrl', function($scope, $ionicPopup, $timeout, $location) {
+  var vm = this;
+
+  vm.completeVoucher = completeVoucher;
+
+  function completeVoucher () {
+    vm.completeModal = $ionicPopup.show({
+      template: '<div><i class="icon ion-android-done"></i></div>' +
+        '<div>Sent</div>',
+      cssClass: 'confirmation'
+    });
+
+    $timeout(function() {
+      vm.completeModal.close();
+      $location.path('/home');
+    }, 680);
+  }
+})
+
+.controller('HomeCtrl', function($http) {
+  var vm = this;
+
+  var now = new Date();
+  var nextHour = new Date(now).setHours(now.getHours() + 1);
+  var twoHoursLater = new Date(now).setMinutes(now.getMinutes() + 2);
+
+  vm.orders = [
+    {
+      id: 0,
+      state: 'pending',
+      name: 'Black Friday',
+      lockedDateTime: nextHour,
+      pickupDateTime: twoHoursLater
+    },
+    {
+      id: 0,
+      state: 'completed',
+      name: 'Tender Green',
+      lockedDateTime: new Date(),
+      pickupDateTime: new Date()
+    },
+    {
+      id: 0,
+      state: 'completed',
+      name: 'Starbucks',
+      lockedDateTime: new Date(),
+      pickupDateTime: new Date()
+    },
+  ];
+
+  vm.getPendingList = getPendingList;
+  vm.getCompletedList = getCompletedList;
+
+  function getPendingList () {
+    return vm.orders.filter(function(order) {
+      return order.state === 'pending';
+    });
+  }
+
+  function getCompletedList () {
+    return vm.orders.filter(function(order) {
+      return order.state === 'completed';
+    });
+  }
+})
+
+.controller('PickupCtrl', function() {
+  var vm = this;
+
+  var brCodeDOM = document.querySelector('#br_code');
+
+  JsBarcode(brCodeDOM, 'test');
 })
