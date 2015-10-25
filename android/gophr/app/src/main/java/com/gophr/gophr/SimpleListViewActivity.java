9e070c606da40c33621a9a22fce357930a6b3fd2
tmitim <timothy@edlio.com>
2015-10-24 17:49:50 -0700

    add columns to list view (android)

--- android/gophr/app/src/main/java/com/gophr/gophr/SimpleListViewActivity.java
+++ android/gophr/app/src/main/java/com/gophr/gophr/SimpleListViewActivity.java
@@ -7,42 +7,73 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import static com.gophr.gophr.Constants.FIRST_COLUMN;
+import static com.gophr.gophr.Constants.FOURTH_COLUMN;
+import static com.gophr.gophr.Constants.SECOND_COLUMN;
+import static com.gophr.gophr.Constants.THIRD_COLUMN;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
 /**
  * Created by tmitim_air on 10/24/15.
  */
 public class SimpleListViewActivity extends Activity {
+//
+//    private ListView mainListView ;
+//    private ArrayAdapter<String> listAdapter ;
 
-    private ListView mainListView ;
-    private ArrayAdapter<String> listAdapter ;
+    private ArrayList<HashMap<String, String>> list;
 
-    /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // Find the ListView resource.
-        mainListView = (ListView) findViewById( R.id.mainListView );
+        ListView listView=(ListView)findViewById(R.id.listView1);
 
-        // Create and populate a List of planet names.
-        String[] planets = new String[] { "Mercury", "Venus", "Earth", "Mars",
-                "Jupiter", "Saturn", "Uranus", "Neptune"};
-        ArrayList<String> planetList = new ArrayList<String>();
-        planetList.addAll( Arrays.asList(planets) );
+        list = new ArrayList<HashMap<String,String>>();
 
-        // Create ArrayAdapter using the planet list.
-        listAdapter = new ArrayAdapter<String>(this, R.layout.simplerow, planetList);
+        HashMap<String,String> temp=new HashMap<String, String>();
+        temp.put(FIRST_COLUMN, "Ankit Karia");
+        temp.put(SECOND_COLUMN, "Male");
+        temp.put(THIRD_COLUMN, "22");
+        temp.put(FOURTH_COLUMN, "Unmarried");
+        list.add(temp);
 
-        // Add more planets. If you passed a String[] instead of a List<String>
-        // into the ArrayAdapter constructor, you must not add more items.
-        // Otherwise an exception will occur.
-        listAdapter.add( "Ceres" );
-        listAdapter.add( "Pluto" );
-        listAdapter.add( "Haumea" );
-        listAdapter.add( "Makemake" );
-        listAdapter.add( "Eris" );
+        HashMap<String,String> temp2=new HashMap<String, String>();
+        temp2.put(FIRST_COLUMN, "Rajat Ghai");
+        temp2.put(SECOND_COLUMN, "Male");
+        temp2.put(THIRD_COLUMN, "25");
+        temp2.put(FOURTH_COLUMN, "Unmarried");
+        list.add(temp2);
 
-        // Set the ArrayAdapter as the ListView's adapter.
-        mainListView.setAdapter( listAdapter );
+        HashMap<String, String> temp3 = new HashMap<String, String>();
+        temp3.put(FIRST_COLUMN, "Karina Kaif");
+        temp3.put(SECOND_COLUMN, "Female");
+        temp3.put(THIRD_COLUMN, "31");
+        temp3.put(FOURTH_COLUMN, "Unmarried");
+        list.add(temp3);
+
+        ListViewAdapter adapter=new ListViewAdapter(this, list);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
+        {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
+            {
+                int pos=position+1;
+                Toast.makeText(SimpleListViewActivity.this, Integer.toString(pos)+" Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+        });
     }
 }
